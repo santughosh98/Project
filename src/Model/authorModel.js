db2a020a9fcf3c8c3f2a9ab54083889f446fc50a
Prakash <prakashku5566singh@gmail.com>
2022-09-09 23:37:47 +0530

    Group 09 project

--- src/Model/authorModel.js
+++ src/Model/authorModel.js
@@ -3,11 +3,12 @@ const authorSchema = new mongoose.Schema({
     fname: {
         type: String,
         required: true,
-        // trim:true
+         trim:true
     },
     lname: {
         type: String,
         required: true,
+        trim:true
     },
     title: {
         type: String,
